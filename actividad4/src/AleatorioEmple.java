cc4d4f2acc291bc8e8dfead173086f42bb89a706
FranLara99 <franlara131099@gmail.com>
2021-02-15 19:28:58 +0100

    Añado una línea de visualización para el método crear

--- actividad4/src/AleatorioEmple.java
+++ actividad4/src/AleatorioEmple.java
@@ -44,6 +44,7 @@ public class AleatorioEmple {
 	}// fin verporconsola
 
 	public static void crear() throws IOException {
+		System.out.println("Añado una línea de visualización para crear()");
 		File fichero = new File("AleatorioEmple.dat");
 		RandomAccessFile file = new RandomAccessFile(fichero, "rw");
 		String apellido[] = { "FERNANDEZ", "GIL", "LOPEZ", "RAMOS", "SEVILLA", "CASILLA", "REY" };
