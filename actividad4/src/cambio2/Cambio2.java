ead093f7e67cd1b375222bf19ee8cf21cf6a4594
FranLara99 <franlara131099@gmail.com>
2021-02-16 12:55:23 +0100

    Sincronizo con el remoto creando un comentario en la clase Cambio2 y haciendo click en commit and push

--- actividad4/src/cambio2/Cambio2.java
+++ actividad4/src/cambio2/Cambio2.java
@@ -2,6 +2,7 @@ package cambio2;
 public class Cambio2 {
 	public static void main (String [] args) {
 		System.out.println("Añado una línea para la clase Cambio2");
+		//comentario en cambio2 para sincronizar con el remoto
 	}
 	
 }
